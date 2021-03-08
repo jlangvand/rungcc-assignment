b5a0ad6659ff9c1cb13bb57254c3f1c32e572313
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-03-08 10:42:30 +0100

    HTML safe util

--- rungcc/utils.py
+++ rungcc/utils.py
@@ -19,9 +19,19 @@ def replace_special_chars(src: str) -> str:
     src = src.replace("%2C", ",")
     src = src.replace("%3A", ":")
     src = src.replace("%3B", ";")
+    src = src.replace("%3F", "?")
     src = src.replace("%5C", "\\")
     src = src.replace("%7B", "{")
     src = src.replace("%7D", "}")
+    src = src.replace("%7E", "~")
     src = src.replace("+", " ")
     src = src.replace("%0D%0A", "\n")
+    return src
+
+
+def html_safe(src: str) -> str:
+    src = src.replace("æ", "&aelig;")
+    src = src.replace("ø", "&oring;")
+    src = src.replace("å", "&aelig")
+    src = src.replace("©", "&copy;")
     return src
